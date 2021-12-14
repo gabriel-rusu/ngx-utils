bffe18300aa4065bc6ffef02e8fcf7d22ae5944b
gabriel-rusu <rusugabriel13@gmail.com>
2021-12-14 13:01:37 +0200

    deleted angular-cli generated files. closes #1

--- projects/ngx-caching/src/public-api.ts
+++ projects/ngx-caching/src/public-api.ts
@@ -1,9 +1,4 @@
 /*
  * Public API Surface of ngx-caching
  */
-
-export * from './lib/ngx-caching.service';
-export * from './lib/ngx-caching.component';
-export * from './lib/ngx-caching.module';
-
 export * from './lib/services/cache/cache.service';
